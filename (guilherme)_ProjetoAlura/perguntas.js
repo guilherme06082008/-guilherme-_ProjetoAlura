87d90aba21a9c604547ab244ead6a5b1e145301f
guilherme06082008 <162757985+guilherme06082008@users.noreply.github.com>
2024-10-04 19:58:09 -0300

    Add files via upload

---
+++ (guilherme)_ProjetoAlura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'Quel era modelo do primeiro carro a andar no Brasil?',
+    'O carro em questão era um Peugeot Type 3'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados o Brasil tem?',
+    'O Brasil tem 26 estados'
+)
+
+criaCartao(
+    'historia',
+    'quantos anos durou a ditadura militar?',
+    'a ditadura durou 21 anos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz marionete em Inglês?',
+    'marionete em ingles é puppet '
+)
